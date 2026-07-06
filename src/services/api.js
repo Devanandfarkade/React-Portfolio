df6c74eb61dcd00066544e92e92c59ba3ff1a0e2
Devanand Farkade <dfarkade330@gmail.com>
2026-07-06 11:14:21 +0530

    some changes did

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
-const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_BASE = import.meta.env.VITE_API_URL;
 
 const fetchAPI = async (endpoint, options = {}) => {
   const url = `${API_BASE}${endpoint}`;
-  
+
   const headers = {
     "Content-Type": "application/json",
     ...options.headers
